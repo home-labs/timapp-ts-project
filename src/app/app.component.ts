f08cd00ed6fdf60abbd659e004921b621f0f1ab5
Rafael Laurindo <rafaelplaurindo@gmail.com>
2018-12-26 16:32:10 -0200

    reorganizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,11 @@ import { NgTimapp } from '../../projects/ng-timapp/src/public_api';
 export class AppComponent {
     title = 'app';
 
-    private calculation: NgTimapp.Calculation;
     private conversor: NgTimapp.Conversion;
-
-    private time: Object = {
-        hours: 0,
-        minutes: 59,
-        seconds: 61
-    };
+    private sumCalc: NgTimapp.Calculation;
+    private diffCalc: NgTimapp.Calculation;
 
     constructor() {
-        this.calculation = new NgTimapp.Calculation(this.time);
-
         // this.conversor = new NgTimapp.Conversion(
         //     {
         //         // minutes: '99'
@@ -30,12 +23,30 @@ export class AppComponent {
         //         seconds: 9734
         //     }
         // );
+
+        // this.sumCalc = new NgTimapp.Calculation(
+        //     {
+        //         hours: 0,
+        //         minutes: 59,
+        //         seconds: 61
+        //     }
+        // );
+
+        this.diffCalc = new NgTimapp.Calculation(
+            {
+                hours: 0,
+                minutes: 0,
+                seconds: 0
+            }
+        );
     }
 
     ngOnInit() {
         // console.log(`${this.conversor.getFormattedHours()}:${this.conversor.getFormattedMinutes()}:${this.conversor.getFormattedSerconds()}`);
 
-        console.log('sum: ', this.calculation.sum({ seconds: 30 }, { seconds: 31 }));
+        // console.log('sum: ', this.sumCalc.sum({ seconds: 30 }, { seconds: 31 }));
+
+        console.log('difference: ', this.diffCalc.difference({ seconds: 30 }));
     }
 
 }
