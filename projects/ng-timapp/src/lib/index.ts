3fdf691f284ca635a3c6294b1f0d2083d004bd5d
Rafael Pereira Laurindo <rafael.laurindo@cicc.local>
2019-07-04 02:16:50 -0300

    improvements

--- projects/ng-timapp/src/lib/index.ts
+++ projects/ng-timapp/src/lib/index.ts
@@ -2,5 +2,3 @@ export { Calculation } from './calculation';
 export { Conversion } from './conversion';
 export { Formatting } from './formatting';
 export { Time } from './time';
-
-export { NgTimappModule } from './module';
--- projects/ng-timapp/src/public_api.ts
+++ projects/ng-timapp/src/public_api.ts
@@ -1,4 +1,2 @@
 import * as NgTimapp from './lib';
 export { NgTimapp };
-
-// export { NgTimappModule } from './lib';
